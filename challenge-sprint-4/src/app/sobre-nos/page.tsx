89aad4526e6caf8d9c80f8f510afbf100918855a
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-11-03 17:21:56 -0300

    pagina sobre nos e sobre projeto funcionando

--- challenge-sprint-4/src/app/sobre-nos/page.tsx
+++ challenge-sprint-4/src/app/sobre-nos/page.tsx
@@ -1,7 +1,18 @@
 export default function SobreNos() {
   return (
-    <div>
-      <h1>SobreNos</h1>
+    <div className="sobre-nos-container flex items-center h-90vh justify-around">
+      <div className="flex flex-col items-center">
+        <h1 className="text-5xl">A solução para o sucesso</h1>
+        <h2 className="text-2xl">O seu conserto de veículo aqui e agora!</h2>
+      </div>
+      <p className="w-[30%]">
+        A iniciativa busca superar o modelo tradicional e conservador do setor,
+        onde a interação física é um pré-requisito para qualquer diagnóstico ou
+        serviço. O diferencial trazido pelo projeto é o uso de Inteligência
+        Artificial para fornecer diagnósticos assertivos, ágeis e confiáveis,
+        garantindo ao cliente uma experiência denominada "phygital" — uma junção
+        do físico com o digital.
+      </p>
     </div>
   );
 }
--- challenge-sprint-4/src/app/sobre-projeto/page.tsx
+++ challenge-sprint-4/src/app/sobre-projeto/page.tsx
@@ -1,7 +1,19 @@
 export default function SobreProjeto() {
   return (
-    <div>
-      <h1>SobreProjeto</h1>
+    <div className="sobre-projeto-container flex items-center h-90vh justify-around">
+      <p className="w-[30%]">
+        O projeto "ConectCar" apresenta uma solução inovadora para o mercado de
+        reparação automotiva, enfocando a experiência digital e a eficiência dos
+        serviços prestados. A proposta central é a criação de um sistema
+        integrado que permite aos clientes do Centro Automotivo Porto a
+        realização de diagnósticos veiculares e a contratação de serviços de
+        reparo de forma online, sem a necessidade de deslocamento até a oficina
+        mecânica para a primeira avaliação.
+      </p>
+      <div className="flex flex-col items-center">
+        <h1 className="text-5xl">A solução para o sucesso</h1>
+        <h2 className="text-2xl">O seu conserto de veículo aqui e agora!</h2>
+      </div>
     </div>
   );
 }
